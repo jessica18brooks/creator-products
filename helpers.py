308cd9dd0417e3c6743595f80b63d4ae0ff96055
jessica18brooks <jessica18brooks@outlook.com>
2022-09-08 20:01:15 +0100

    Adding schema and load config

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import json
 import yaml
 import logging
 
@@ -22,6 +23,14 @@ def read_yaml(file):
         logging.error(f'No "variables" defined in package_vars')
 
 
+def generate_schema(json_data):
+    schema_array = []
+    for s in json_data:
+        schema_array.append(bigquery.SchemaField(s['name'], s['type'], mode=s['mode']))
+
+    return schema_array
+
+
 def upload_dataframe_to_bigquery(dataframe, variables):
     bq_client = bigquery.Client()
 
@@ -44,10 +53,29 @@ def upload_dataframe_to_bigquery(dataframe, variables):
         logging.error(f'No table_id defined in package_vars')
         return response_message(True, 400, 'No table_id defined in package_vars')
 
-    job_config = bigquery.LoadJobConfig()  # TODO: Set up schema for load job
+    try:
+        with open('config/SampleDataSchema.json', 'r') as j:
+            schema = generate_schema(json.load(j))
+    except OSError:
+        logging.error(f'Failed to open json schema')
+        return response_message(True, 400, 'Failed to open json schema')
+
+    try:
+        table = bq_client.create_table(bigquery.Table(table_ref, schema), exists_ok=True)
+    except Exception as e:
+        logging.error(f'Failed to create BigQuery table {table_ref} with error {e.args}')
+        return response_message(True, 400, 'Failed to create BigQuery table')
+
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.CSV
+    job_config.field_delimiter = ','
+    job_config.schema = schema
+    job_config.autodetect = False
+    job_config.skip_leading_rows = 1
+    job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
 
     job = bq_client.load_table_from_dataframe(
-        dataframe, table_ref, job_config=job_config
+        dataframe, table, job_config=job_config
     )
     job.result()  # Wait for the job to complete.
 
